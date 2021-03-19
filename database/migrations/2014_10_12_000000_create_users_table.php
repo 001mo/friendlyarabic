0c784762acbedfca40eaafe2bc1c9dec94f7c70a
001mo <ahmedmohamadkhalaf@gmail.com>
2021-03-19 06:51:14 +0200

    customized roles with users model & migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
+            $table->string('phone')->nullable();
             $table->string('email')->unique();
+            $table->string('role')->default('student');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
