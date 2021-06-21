bb43da50da0848b8acc1082b46b5043dc1dda1b3
001mo <ahmedmohamadkhalaf@gmail.com>
2021-06-21 02:02:01 +0200

    teachers list

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ Route::get('/', function () {
 
 Route::get('/signup', function(){ return view('signup'); })->name('signup');
 Route::get('/login', function(){ return view('login'); })->name('login');
+
+Route::get('/t', function(){ return view('t'); });
